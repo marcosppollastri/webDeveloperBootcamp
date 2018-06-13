67c46f75013eed5b4dc22da8b1cfdaaa2ff6544b
Marcos Peña Pollastri <marcos@marcospena.org>
2018-06-13 12:28:30 -0300

    Agregado nueva sección backend Committer: Marcos Peña Pollastri

---
+++ backend/IntroToCommandLine/ex1.sh
@@ -0,0 +1,15 @@
+mkdir Animals
+mkdir ./Animals/AwesomeAnimals
+mkdir ./Animals/SketchyAnimals
+touch ./Animals/AwesomeAnimals/Capybara.js
+touch ./Animals/AwesomeAnimals/ArticFox.html
+touch ./Animals/AwesomeAnimals/TreeFrog.txt
+touch ./Animals/SketchyAnimals/BrownRecluse.html
+touch ./Animals/SketchyAnimals/BulletAnt.js
+mkdir ./Animals/SketchyAnimals/Snakes
+touch ./Animals/SketchyAnimals/Snakes/Cobra.css
+touch ./Animals/SketchyAnimals/Snakes/ReticulatedPython.py
+
+#rm ./Animals/SketchyAnimals/BulletAnt.js
+#rm -rf ./Animals
+
