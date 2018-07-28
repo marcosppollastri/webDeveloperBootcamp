135dc86b4f8b418b8dc713fc0f250e6236b244e9
Marcos J. Peña Pollastri <marcos@marcospena.org>
2018-07-28 04:23:50 -0300

    Terminada v1 de yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -6,7 +6,13 @@ var campgrounds = [
     {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3103/2677233699_de6e456567_o_d.jpg"},
     {name: "Granite Hill", image: "https://farm5.staticflickr.com/4248/34589721790_caf326a52a_o_d.jpg"},
     {name: "Montain Goat's Rest", image: "https://farm4.staticflickr.com/3867/14423528780_d31ea2794a_o_d.jpg"},
-    {name: "Talampaya Vay", image: "https://farm4.staticflickr.com/3837/14679255190_95ebe52754_o_d.jpg"}
+    {name: "Talampaya Vay", image: "https://farm4.staticflickr.com/3837/14679255190_95ebe52754_o_d.jpg"},
+    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3103/2677233699_de6e456567_o_d.jpg"},
+    {name: "Granite Hill", image: "https://farm5.staticflickr.com/4248/34589721790_caf326a52a_o_d.jpg"},
+    {name: "Montain Goat's Rest", image: "https://farm4.staticflickr.com/3867/14423528780_d31ea2794a_o_d.jpg"},
+    {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3103/2677233699_de6e456567_o_d.jpg"},
+    {name: "Granite Hill", image: "https://farm5.staticflickr.com/4248/34589721790_caf326a52a_o_d.jpg"},
+    {name: "Montain Goat's Rest", image: "https://farm4.staticflickr.com/3867/14423528780_d31ea2794a_o_d.jpg"}
     
 ];
 
